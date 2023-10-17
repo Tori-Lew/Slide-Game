e0448350a1fe2a272471f58f1498a7170d72e50f
Tori Lew <55900873+Tori-Lew@users.noreply.github.com>
2023-10-16 17:13:11 -0700

    Add files

---
+++ Slide Game/Slide_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slide_GameApp.swift
+//  Slide Game
+//
+//  Created by Student Account on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Slide_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
